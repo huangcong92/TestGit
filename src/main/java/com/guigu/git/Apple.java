83c853be34750962d8f4dade860e5600c0545fa4
huangcong <314606810@qq.com>
2020-02-14 00:23:42 +0800

    我第一次提交

---
+++ src/main/java/com/guigu/git/Apple.java
@@ -0,0 +1,27 @@
+/**
+ * Project Name:TestGit
+ * File Name:Apple.java
+ * Package Name:com.guigu.git
+ * Date:обнГ11:50:41
+ * Copyright (c) 2020, bluemobi All Rights Reserved.
+ *
+*/
+
+package com.guigu.git;
+/**
+ * Description:	   <br/>
+ * Date:     обнГ11:50:41 <br/>
+ * @author   huangcong
+ * @version  
+ * @see 	 
+ */
+public class Apple {
+
+    public Apple() {
+
+        //  Auto-generated constructor stub
+
+    }
+
+}
+
---
+++ src/test/java/com/guigu/test/GoodTest.java
@@ -0,0 +1,27 @@
+/**
+ * Project Name:TestGit
+ * File Name:GoodTest.java
+ * Package Name:com.guigu.test
+ * Date:обнГ11:52:13
+ * Copyright (c) 2020, bluemobi All Rights Reserved.
+ *
+*/
+
+package com.guigu.test;
+/**
+ * Description:	   <br/>
+ * Date:     обнГ11:52:13 <br/>
+ * @author   huangcong
+ * @version  
+ * @see 	 
+ */
+public class GoodTest {
+
+    public GoodTest() {
+
+        //  Auto-generated constructor stub
+
+    }
+
+}
+
