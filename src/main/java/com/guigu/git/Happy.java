d50f2f63190fa7b18ca26111dbf7cb8f1efce5cc
huangcong <314606810@qq.com>
2020-02-14 00:25:12 +0800

    第二次提交

---
+++ src/main/java/com/guigu/git/Happy.java
@@ -0,0 +1,27 @@
+/**
+ * Project Name:TestGit
+ * File Name:Happy.java
+ * Package Name:com.guigu.git
+ * Date:ионГ12:24:15
+ * Copyright (c) 2020, bluemobi All Rights Reserved.
+ *
+*/
+
+package com.guigu.git;
+/**
+ * Description:	   <br/>
+ * Date:     ионГ12:24:15 <br/>
+ * @author   huangcong
+ * @version  
+ * @see 	 
+ */
+public class Happy {
+
+    public Happy() {
+
+        //  Auto-generated constructor stub
+
+    }
+
+}
+
